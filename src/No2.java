3ec433c361372a32348d11a5deb5892d39ea8f21
Muhamad Fikri <muhamad.fikri@unida.ac.id>
2022-01-09 19:56:56 +0700

    Tugas_individu4

---
+++ src/No2.java
@@ -0,0 +1,11 @@
+public class No2 {
+	public static void main(String[] args){
+		int a, b;
+		for (a = 1; a <= 5; a++){
+			for (b = 5; b >= a; b--){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/No3.java
@@ -0,0 +1,15 @@
+public class No3 {
+	public static void main (String args[])
+	{
+		int a; 
+		int b;
+		int n = 5;
+		for (a = 1; a <= n; a++){
+			for (b = 1; b <= a; b++){
+				System.out.print(b+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+}
